4a2bb048b71d7c8686fd140500c7fb7eac7b05fb
叶晨 <seuyechen@gmail.com>
2018-07-17 10:21:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ from glob import glob
 
 import site
 
-if 'BOARD' not in os.environ or os.environ['BOARD'] != 'Pynq-Z1':
-    print("Only supported on a Pynq-Z1 Board")
+if 'BOARD' not in os.environ or os.environ['BOARD'] != 'Pynq-Z2':
+    print("Only supported on a Pynq-Z2 Board")
     exit(1)
 
 package_data = []
